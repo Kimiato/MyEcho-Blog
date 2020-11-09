fb69e9c47b899414a59219cab9972fee53ea24d6
kimiato <853447650@qq.com>
2020-11-09 21:48:47 +0800

    fix: token认证修改

--- myecho/controllers/authentication.py
+++ myecho/controllers/authentication.py
@@ -2,6 +2,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from django.utils.translation import ugettext_lazy as _
 from functools import update_wrapper
+from rest_framework import exceptions
+from rest_framework.authentication import get_authorization_header
 
 from sso.models import Token
 
@@ -38,7 +40,25 @@ class BaseTokenAuthentication(TokenAuthentication):
 
     def authenticate(self, request):
         self.request = request
-        return super().authenticate(request)
+        auth = get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != self.keyword.lower().encode():
+            raise AuthenticationFailed(_('未登录！'))
+
+        if len(auth) == 1:
+            msg = _('Invalid token header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid token header. Token string should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            token = auth[1].decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        return self.authenticate_credentials(token)
 
     def _get_user_by_token_key(self, key):
         token = Token.objects.filter(key=key).first()
